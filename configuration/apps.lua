09283cc20fbc89aed6b7b79caaf850f330252d3b
Abel Binoop <abelbinoop@live.co.uk>
2020-05-06 21:18:38 +0100

    Add redshift to apps.lua

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -39,6 +39,7 @@ return {
 
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
-    '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
+    '~/.config/awesome/configuration/awspawn', -- Spawn "dirty" apps that can linger between sessions
+    'redshift -O 4500K'
   }
 }
